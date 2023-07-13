241662c851b0dabb4d54b4b3fe6b60181bb7acf8
Demigodrick <117108241+Demigodrick@users.noreply.github.com>
2023-07-13 20:20:49 +0100

    lemmy customization

--- src/helpers/lemmy.ts
+++ src/helpers/lemmy.ts
@@ -8,7 +8,7 @@ import {
 export const LEMMY_SERVERS =
   "CUSTOM_LEMMY_SERVERS" in window
     ? (window.CUSTOM_LEMMY_SERVERS as string[])
-    : ["lemmy.world", "lemmy.ml", "beehaw.org", "sh.itjust.works"];
+    : ["lemmy.zip", "lemmy.world"];
 
 export interface LemmyJWT {
   sub: number;
